c675aae145bebf31b82c4dc663255b0959d95a6a
dark <guruprasadb3556@gmail.com>
2026-02-06 18:04:36 +0530

    CMS-101: initial spring boot project setup

---
+++ src/main/java/com/company/cms/ChatgptProjectApplication.java
@@ -0,0 +1,13 @@
+package com.company.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatgptProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatgptProjectApplication.class, args);
+	}
+
+}
